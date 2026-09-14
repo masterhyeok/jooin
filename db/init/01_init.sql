998db3a84cef7cbef1fa8fb689cf2d0e3aaa9fb5
bellround <rainarmy11@gmail.com>
2026-09-14 17:24:59 +0900

    Revert "a"
    This reverts commit 11a9c7d14786567f09eabf2098afe47b76291ae1.

--- db/init/01_init.sql
+++
@@ -1,7 +0,0 @@
-create database if not exists shopping_db character set utf8mb4 collate utf8mb4_general_ci;
-
-create user if not exists 'nada'@'localhost' identified by 'gun';
-create user if not exists 'nada'@'%' identified by 'gun';
-
-grant select, insert, update, delete on shopping_db.* to 'nada'@'%';
-flush privileges;
